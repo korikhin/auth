bfbeaed03ddf2a8b821a1ed77a9e8e4412ad8730
korikhin <korikhin666@gmail.com>
2024-01-31 15:51:36 +0700

    chore(cors): refactor CORS configuration handling
    Refactored the CORS configuration to use a more descriptive parameter name 'c' instead of 'config'. Updated the allowed headers and added support for CSRF token. This change aims to enhance code readability and maintainability.

--- internal/lib/http/cors/cors.go
+++ internal/lib/http/cors/cors.go
@@ -9,30 +9,39 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-func New(config config.CORS) func(http.Handler) http.Handler {
+func New(c config.CORS) func(http.Handler) http.Handler {
 	return handlers.CORS(
-		handlers.AllowedOrigins(config.AllowedOrigins),
+		handlers.AllowedOrigins(c.AllowedOrigins),
 		handlers.AllowedMethods([]string{
+			http.MethodOptions,
 			http.MethodGet,
 			http.MethodPost,
 			http.MethodPut,
 			http.MethodDelete,
 		}),
 		handlers.AllowedHeaders([]string{
+			httplib.HeaderAccept,
+			httplib.HeaderAcceptEncoding,
 			httplib.HeaderAuth,
+			httplib.HeaderAuthorization,
 			httplib.HeaderCacheControl,
+			httplib.HeaderContentLength,
 			httplib.HeaderContentType,
+			httplib.HeaderCustomHeader,
 			httplib.HeaderDNT,
 			httplib.HeaderIfModifiedSince,
 			httplib.HeaderKeepAlive,
+			httplib.HeaderOrigin,
 			httplib.HeaderRange,
 			httplib.HeaderRequestedWith,
+			httplib.HeaderRequiredRole,
 			httplib.HeaderUserAgent,
+			httplib.HeaderCSRFToken,
 		}),
 		handlers.ExposedHeaders([]string{
 			httplib.HeaderRequestID,
 		}),
 		handlers.AllowCredentials(),
-		handlers.MaxAge(config.MaxAge),
+		handlers.MaxAge(c.MaxAge),
 	)
 }
--- internal/lib/http/http.go
+++ internal/lib/http/http.go
@@ -6,15 +6,21 @@ import (
 
 // Headers
 const (
+	HeaderAccept          = "Accept"
+	HeaderAcceptEncoding  = "Accept-Encoding"
 	HeaderAuth            = "Authorization"
+	HeaderAuthorization   = "Authorization"
 	HeaderCacheControl    = "Cache-Control"
+	HeaderContentLength   = "Content-Length"
 	HeaderContentType     = "Content-Type"
 	HeaderDNT             = "DNT"
 	HeaderIfModifiedSince = "If-Modified-Since"
 	HeaderKeepAlive       = "Keep-Alive"
+	HeaderOrigin          = "Origin"
 	HeaderRange           = "Range"
 	HeaderUserAgent       = "User-Agent"
 
+	HeaderCSRFToken     = "X-CSRF-Token"
 	HeaderCustomHeader  = "X-CustomHeader"
 	HeaderRequestedWith = "X-Requested-With"
 	HeaderRequestID     = "X-Request-ID"
